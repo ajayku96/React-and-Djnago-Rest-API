8009178cf1f2fc8e1c5e2908950629f78a9035c3
Ajay kumar <ajayku96@gmail.com>
2021-07-13 23:57:42 +0530

    Update settings.py

--- REST-API Django/ZomatoAPI/ZomatoAPI/settings.py
+++ REST-API Django/ZomatoAPI/ZomatoAPI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ye(r(wqwsj4(u4f60jslevxbzyxa5cqn(4dbl@ou56r*wu_l6o'
+SECRET_KEY = 'EnterYourSecretKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
